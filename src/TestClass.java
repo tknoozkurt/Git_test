ef257c19cdb3e17775d5425dba2a1177743b6a90
tknoozkurt <67170670+tknoozkurt@users.noreply.github.com>
2020-06-20 05:23:00 -0400

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,7 @@
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println("gitten merhaba");
+        System.out.println("gitten merhabalar hello gitty");
+        int i=7;
     }
 }
